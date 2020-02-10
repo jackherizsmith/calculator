e10af7cab08f662dbb97979ae427266fa752c434
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-02-10 23:27:51 +0000

    fixed problems with minus operator

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const   gridPoints = [[3,1],[3,2],[3,3],[3,4],[3,5],
                       [5,1],[5,2],[5,3],[5,4],[5,5],
                       [6,1],[6,2],[6,3],[6,4],[6,5],
                       [7,1],[7,2],[7,3],[7,4],[7,5]],
-        arrayPosition = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]; // for hard mode...
-let currentNumber, processText, decInNumber, calcComplete, sign, rand,
+        numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]; // for hard mode...
+let currentNumber, processText, decInNumber, calcComplete, sign, arrayPosition,
 memFull = false, memStore = 0, hardMode = false;
 
 allClear();
 
-function allClear() { //toggle hard mode (long press)
+function allClear() {
     processText   = '';
     sign          = '';
     currentNumber = '0';
     decInNumber   = false;
     calcComplete  = true;
-    rand = arrayPosition.slice();
     updateScreen();
 }
 
@@ -25,33 +24,34 @@ function updateScreen() {
     document.getElementById("calcProcessText").innerHTML = processText;
     document.getElementById("currentSign").innerHTML     = sign;
     document.getElementById("calcScreenText").innerHTML  = currentNumber;
-    if (hardMode) {rand = shuffleArray(rand);}
-    else {rand = arrayPosition.slice();}
-    document.getElementById("memStatus").style.gridArea       = `${gridPoints[rand[0]][0]}  / ${gridPoints[rand[0]][1]}  / span 1 / span 1`;
-    document.getElementById("addToMemory").style.gridArea     = `${gridPoints[rand[1]][0]}  / ${gridPoints[rand[1]][1]}  / span 1 / span 1`;
-    document.getElementById("recallFromMemory").style.gridArea= `${gridPoints[rand[2]][0]}  / ${gridPoints[rand[2]][1]}  / span 1 / span 1`;
-    document.getElementById("clearMemory").style.gridArea     = `${gridPoints[rand[3]][0]}  / ${gridPoints[rand[3]][1]}  / span 1 / span 1`;
-    document.getElementById("backspace").style.gridArea       = `${gridPoints[rand[4]][0]}  / ${gridPoints[rand[4]][1]}  / span 1 / span 1`;
-    document.getElementById("squareRoot").style.gridArea      = `${gridPoints[rand[5]][0]}  / ${gridPoints[rand[5]][1]}  / span 1 / span 1`;
-    document.getElementById("seven").style.gridArea           = `${gridPoints[rand[6]][0]}  / ${gridPoints[rand[6]][1]}  / span 1 / span 1`;
-    document.getElementById("eight").style.gridArea           = `${gridPoints[rand[7]][0]}  / ${gridPoints[rand[7]][1]}  / span 1 / span 1`;
-    document.getElementById("nine").style.gridArea            = `${gridPoints[rand[8]][0]}  / ${gridPoints[rand[8]][1]}  / span 1 / span 1`;
-    document.getElementById("divide").style.gridArea          = `${gridPoints[rand[9]][0]}  / ${gridPoints[rand[9]][1]}  / span 1 / span 1`;
-    document.getElementById("inverseSign").style.gridArea     = `${gridPoints[rand[10]][0]} / ${gridPoints[rand[10]][1]} / span 1 / span 1`;
-    document.getElementById("four").style.gridArea            = `${gridPoints[rand[11]][0]} / ${gridPoints[rand[11]][1]} / span 1 / span 1`;
-    document.getElementById("five").style.gridArea            = `${gridPoints[rand[12]][0]} / ${gridPoints[rand[12]][1]} / span 1 / span 1`;
-    document.getElementById("six").style.gridArea             = `${gridPoints[rand[13]][0]} / ${gridPoints[rand[13]][1]} / span 1 / span 1`;
-    document.getElementById("times").style.gridArea           = `${gridPoints[rand[14]][0]} / ${gridPoints[rand[14]][1]} / span 1 / span 1`;
-    document.getElementById("infinity").style.gridArea        = `${gridPoints[rand[15]][0]} / ${gridPoints[rand[15]][1]} / span 1 / span 1`;
-    document.getElementById("one").style.gridArea             = `${gridPoints[rand[16]][0]} / ${gridPoints[rand[16]][1]} / span 1 / span 1`;
-    document.getElementById("two").style.gridArea             = `${gridPoints[rand[17]][0]} / ${gridPoints[rand[17]][1]} / span 1 / span 1`;
-    document.getElementById("three").style.gridArea           = `${gridPoints[rand[18]][0]} / ${gridPoints[rand[18]][1]} / span 1 / span 1`;
-    document.getElementById("minus").style.gridArea           = `${gridPoints[rand[19]][0]} / ${gridPoints[rand[19]][1]} / span 1 / span 1`;
-    document.getElementById("allClear").style.gridArea        = `${gridPoints[rand[20]][0]} / ${gridPoints[rand[20]][1]} / span 1 / span 1`;
-    document.getElementById("zero").style.gridArea            = `${gridPoints[rand[21]][0]} / ${gridPoints[rand[21]][1]} / span 1 / span 1`;
-    document.getElementById("addDecimal").style.gridArea      = `${gridPoints[rand[22]][0]} / ${gridPoints[rand[22]][1]} / span 1 / span 1`;
-    document.getElementById("equals").style.gridArea          = `${gridPoints[rand[23]][0]} / ${gridPoints[rand[23]][1]} / span 1 / span 1`;
-    document.getElementById("add").style.gridArea             = `${gridPoints[rand[24]][0]} / ${gridPoints[rand[24]][1]} / span 1 / span 1`;
+    
+    if (hardMode) {arrayPosition = shuffleArray(numbers.slice())}
+    else {arrayPosition = numbers.slice();}
+    document.getElementById("memStatus").style.gridArea       = `${gridPoints[arrayPosition[0]][0]}  / ${gridPoints[arrayPosition[0]][1]}  / span 1 / span 1`;
+    document.getElementById("addToMemory").style.gridArea     = `${gridPoints[arrayPosition[1]][0]}  / ${gridPoints[arrayPosition[1]][1]}  / span 1 / span 1`;
+    document.getElementById("recallFromMemory").style.gridArea= `${gridPoints[arrayPosition[2]][0]}  / ${gridPoints[arrayPosition[2]][1]}  / span 1 / span 1`;
+    document.getElementById("clearMemory").style.gridArea     = `${gridPoints[arrayPosition[3]][0]}  / ${gridPoints[arrayPosition[3]][1]}  / span 1 / span 1`;
+    document.getElementById("backspace").style.gridArea       = `${gridPoints[arrayPosition[4]][0]}  / ${gridPoints[arrayPosition[4]][1]}  / span 1 / span 1`;
+    document.getElementById("squareRoot").style.gridArea      = `${gridPoints[arrayPosition[5]][0]}  / ${gridPoints[arrayPosition[5]][1]}  / span 1 / span 1`;
+    document.getElementById("seven").style.gridArea           = `${gridPoints[arrayPosition[6]][0]}  / ${gridPoints[arrayPosition[6]][1]}  / span 1 / span 1`;
+    document.getElementById("eight").style.gridArea           = `${gridPoints[arrayPosition[7]][0]}  / ${gridPoints[arrayPosition[7]][1]}  / span 1 / span 1`;
+    document.getElementById("nine").style.gridArea            = `${gridPoints[arrayPosition[8]][0]}  / ${gridPoints[arrayPosition[8]][1]}  / span 1 / span 1`;
+    document.getElementById("divide").style.gridArea          = `${gridPoints[arrayPosition[9]][0]}  / ${gridPoints[arrayPosition[9]][1]}  / span 1 / span 1`;
+    document.getElementById("inverseSign").style.gridArea     = `${gridPoints[arrayPosition[10]][0]} / ${gridPoints[arrayPosition[10]][1]} / span 1 / span 1`;
+    document.getElementById("four").style.gridArea            = `${gridPoints[arrayPosition[11]][0]} / ${gridPoints[arrayPosition[11]][1]} / span 1 / span 1`;
+    document.getElementById("five").style.gridArea            = `${gridPoints[arrayPosition[12]][0]} / ${gridPoints[arrayPosition[12]][1]} / span 1 / span 1`;
+    document.getElementById("six").style.gridArea             = `${gridPoints[arrayPosition[13]][0]} / ${gridPoints[arrayPosition[13]][1]} / span 1 / span 1`;
+    document.getElementById("times").style.gridArea           = `${gridPoints[arrayPosition[14]][0]} / ${gridPoints[arrayPosition[14]][1]} / span 1 / span 1`;
+    document.getElementById("infinity").style.gridArea        = `${gridPoints[arrayPosition[15]][0]} / ${gridPoints[arrayPosition[15]][1]} / span 1 / span 1`;
+    document.getElementById("one").style.gridArea             = `${gridPoints[arrayPosition[16]][0]} / ${gridPoints[arrayPosition[16]][1]} / span 1 / span 1`;
+    document.getElementById("two").style.gridArea             = `${gridPoints[arrayPosition[17]][0]} / ${gridPoints[arrayPosition[17]][1]} / span 1 / span 1`;
+    document.getElementById("three").style.gridArea           = `${gridPoints[arrayPosition[18]][0]} / ${gridPoints[arrayPosition[18]][1]} / span 1 / span 1`;
+    document.getElementById("minus").style.gridArea           = `${gridPoints[arrayPosition[19]][0]} / ${gridPoints[arrayPosition[19]][1]} / span 1 / span 1`;
+    document.getElementById("allClear").style.gridArea        = `${gridPoints[arrayPosition[20]][0]} / ${gridPoints[arrayPosition[20]][1]} / span 1 / span 1`;
+    document.getElementById("zero").style.gridArea            = `${gridPoints[arrayPosition[21]][0]} / ${gridPoints[arrayPosition[21]][1]} / span 1 / span 1`;
+    document.getElementById("addDecimal").style.gridArea      = `${gridPoints[arrayPosition[22]][0]} / ${gridPoints[arrayPosition[22]][1]} / span 1 / span 1`;
+    document.getElementById("equals").style.gridArea          = `${gridPoints[arrayPosition[23]][0]} / ${gridPoints[arrayPosition[23]][1]} / span 1 / span 1`;
+    document.getElementById("add").style.gridArea             = `${gridPoints[arrayPosition[24]][0]} / ${gridPoints[arrayPosition[24]][1]} / span 1 / span 1`;
 }
 
 function shuffleArray(array) {                          // a JavaScript implementation of the Durstenfeld shuffle, a computer-optimized version of Fisher-Yate
@@ -61,7 +61,7 @@ function shuffleArray(array) {                          // a JavaScript implemen
         array[i] = array[j];
         array[j] = temp;
     }
-  return array; 
+    return array; 
 }
 
 function roundNumber(){
@@ -86,27 +86,23 @@ function invSign() {
 
 // PROCESSING INPUTS
 
-function operate(op) { // minus makes '0-' not negative sign
-    if ((currentNumber == 0) && processText !== '') {
-        if (op === ' -') {
-            invSign();
-            calcComplete = false;
-        }
-        else {processText = processText.slice(0,processText.length-2) + op;}
-    }
+function operate(op) {
+    if ((currentNumber == 0) && (op === ' -')) {invSign();}
+    else if (processText !== '' && op !== ' -') {processText = processText.slice(0,processText.length-2) + op;}
     else if (currentNumber !== '0.') {
         calculate();
         processText   = sign + currentNumber + op;
         sign          = '';
         currentNumber = '0';
         decInNumber   = false;
-        calcComplete  = false;
     }
+    calcComplete = false;
     updateScreen();
 }
 
-function newNum(n){//new numbers keep negativity - THE ONLY REAL PROBLEM
-    if ((currentNumber.length < 9 && currentNumber === '0') || calcComplete == true) {
+function newNum(n){
+    if (calcComplete == true) {sign = '';}
+    if (currentNumber === '0' || calcComplete == true) {
         currentNumber = n;
         calcComplete  = false;
     } 
@@ -129,7 +125,6 @@ function addDec() {
 }
 
 function infinity() {
-    sign = '';
     currentNumber = Infinity;
     calcComplete  = true;
     updateScreen();
@@ -137,7 +132,7 @@ function infinity() {
 
 // MEMORY FUNCTIONS
 
-function memAdd() { //add animation to memStatus
+function memAdd() { //add animation to memStatus. Set CSSKeyFrameRule(?) to growing circle fading to transparent and overflow:hidden on me
     if (currentNumber !== 0 && currentNumber !== '0' && currentNumber !== '0.' && currentNumber !== 'Stop it.') {
         calculate();
         document.getElementById("memStatus").style.backgroundColor = '#33CC00';
@@ -182,15 +177,15 @@ function backspace(){
 
 function sqRt() { 
     if (currentNumber !== 0) {
-        calculate();
-        if (sign === '-') {
-            processText  = 'Not an imaginary calculator';
+        if (sign == '-') {
+            processText = 'Not an imaginary calculator';
         }
         else {
+            calculate();
             processText   = '';
-            currentNumber = Math.pow(currentNumber, 0.5)
+            currentNumber = Math.pow(currentNumber, 0.5);
+            roundNumber();
         }
-        roundNumber();
         updateScreen();
         processText = '';
         calcComplete = true;
